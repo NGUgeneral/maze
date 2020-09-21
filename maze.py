cb7b777f662546330b63ea4b895a51f216fd477f
NGUgeneral <egor.osaulenko@gmail.com>
2020-09-21 17:35:03 +0300

    Implemented demo for rewriting existing rows instead of rendering on new lines;

--- maze.py
+++ maze.py
@@ -1,13 +1,22 @@
 from typing import Dict, List
 import copy
+from time import sleep
 
 # ----- START AREA: DATA MODEL
 
-class Cell:
+_R_TEST_FLAG = True
 
-	_SYMBOL = []
-	_mark = ' '
-	_maze = None
+def rewrite_last_n_rows(string, rows):
+	prefix = '\n' + rows * u'\u008D' 
+	print(f"{prefix}{string}")
+
+class Cell:
+	_SYMBOL = None
+	_mark_00= ' '
+	_mark_01= ' '
+	_mark_10= ' '
+	_mark_11= ' '
+	_maze = None # NEED TO GET RID OF IT
 	coord_i = -1
 	coord_j = -1
 	side_n = False
@@ -32,15 +41,15 @@ class Cell:
 		self.side_s = side_s
 		self.side_w = side_w
 
+		self._symbol_updated = True
+
 
 	def symbol(self) -> List[List[str]]:
-		# TODO: SAVE SYMBOL STATE
-		if not self._SYMBOL:
-			print(f'I am about to build symbol for cell {self.coord_i}:{self.coord_j}')
+		if self._symbol_updated:
 			symbol = [
 				[' ', ' ', ' ', ' ', ' '],
-				[' ', ' ', self._mark, ' ', ' '],
-				[' ', ' ', ' ', ' ', ' ']
+				[' ', self._mark_00, self._mark_01, ' ', ' '],
+				[' ', self._mark_10, self._mark_11, ' ', ' ']
 			]
 			if self.side_e:
 				for smb in symbol:
@@ -56,28 +65,41 @@ class Cell:
 				for smb in symbol:
 					smb[-1] += '\n'
 			self._SYMBOL = symbol
+			self._symbol_updated = False
 		return self._SYMBOL
-	# TODO: BUILT-IN CELL MARK SUPPORT
 
-	def place_mark(self, mark: str):
-		if not len(mark) == 1:
-			raise ValueError('Invalid mark provided to place')
-		self._mark = mark
+	def place_character(self, _2x2):
+		self._mark_00 = _2x2[0]
+		self._mark_01 = _2x2[1]
+		self._mark_10 = _2x2[2]
+		self._mark_11 = _2x2[3]
+		self._symbol_updated = True
 
-	def clear_mark(self):
-		self._mark = ' '
+	def remove_character(self):
+		self._mark_00 = ' '
+		self._mark_01 = ' '
+		self._mark_10 = ' '
+		self._mark_11 = ' '
+		self._symbol_updated = True
+
+	def place_player(self):
+		self.place_character(['/', '\\', '\\', '/'])
+
+	def place_enemy(self):
+		self.place_character(['\\', '/', '/', '\\'])
+
+	def place_finish(self):
+		self.place_character(['+', '+', '+', '+'])
 
 class Maze:
 
 	_MAZE_DATA = {}
 
 	def __init__(self, maze: Dict[int, Dict[int, Cell]]):
-		print('init maze')
 		self.frame_maze(maze)
 		self._MAZE_DATA = maze
 
 	def frame_maze(self, maze: Dict[int, Dict[int, Cell]]):
-		print('I am about to build maze frame')
 		size = len(maze.keys())
 		for i in maze:
 			for j in maze[i]:
@@ -98,6 +120,9 @@ class Maze:
 
 	def get_maze_size(self):
 		return len(self._MAZE_DATA.keys())
+
+	def get_maze_cell_height(self):
+		return 3
 
 
 # ----- END AREA: DATA MODEL
@@ -122,9 +147,9 @@ def render_maze(maze: Maze) -> str:
 					print_row[c] += symbol[c]
 					c += 1
 		print_rows.append(print_row)
-	return ''.join(flatten_nested_list(print_rows))
+	return ''.join(flatten_nested_list(print_rows))[:-1]
 
-# ----- START AREA: LOGIC / VIEW
+# ----- END AREA: LOGIC / VIEW
 
 # ----- START AREA: UTILS
 
@@ -170,14 +195,57 @@ maze_cells_data = {
 
 # ----- START AREA: EXECUTE SEQUENCE
 
-answer = ''
 maze = Maze(maze_cells_data)
-maze_cells_data[0][0].place_mark('*')
-while not (answer in ['N', 'n']):  
-	answer = input('Would you like to render maze? (Y/N):\n')
-	if answer in ['Y', 'y']:
-		maze_cells_data[0][0].clear_mark()
+steps = 0
+if _R_TEST_FLAG:
+	_maze_height = maze.get_maze_size() * maze.get_maze_cell_height() + 2
+	_PLAYER_LOCATION = (0,0)
+	x, y = _PLAYER_LOCATION
+	_FINISH_LOCATION = (2,2)
+	maze_cells_data[_FINISH_LOCATION[1]][_FINISH_LOCATION[0]].place_finish()
+	state = 0 # convert to enum
+	for i in range(10):
+		if (y, x) == _FINISH_LOCATION:
+			state = 1
+		if state == 1:
+			break
+		maze_cells_data[x][y].remove_character()
+		if i == 0:
+			y, x = 0, 0
+		if  i == 1:
+			y, x = 0, 1
+		if  i == 2:
+			y, x = 0, 2
+		if  i == 3:
+			y, x = 1, 2
+		if i == 4:
+			y, x = 1, 1
+		if i == 5:
+			y, x = 1, 0
+		if i == 6:
+			y, x = 2, 0
+		if i == 7:
+			y, x = 2, 1
+		if i == 8:
+			y, x = 2, 2
+		maze_cells_data[x][y].place_player()
 		view = render_maze(maze)
-		print(view)
+		if i > 0:
+			rewrite_last_n_rows(view, _maze_height + 1)
+		else:
+			print(view)
+		steps += 1
+		print(f'STEPS: {steps}')
+		sleep(0.5)
+	if state == 1:
+		rewrite_last_n_rows('\n\n\n\n\n\t\t\t=== V I C T O R Y ===\n\n\n\n', _maze_height + 1)
+		print(f'STEPS: {steps}')
+else:
+	answer = ''
+	while not (answer in ['N', 'n']):  
+		answer = input('Would you like to render maze? (Y/N):\n')
+		if answer in ['Y', 'y']:
+			view = render_maze(maze)
+			print(view)
 
 # ----- END AREA: EXECUTE SEQUENCE
